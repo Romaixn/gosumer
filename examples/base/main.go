cdd4749730416f6185edb7c336b1bda1d7bd0908
Romain <romain.herault@les-tilleuls.coop>
2023-06-21 11:57:09 +0200

    docs: improving default example

--- examples/base/main.go
+++ examples/base/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/romaixn/gosumer"
@@ -31,5 +32,26 @@ func main() {
 func processMessage(message any, err chan error) {
 	log.Printf("Message received: %v", message)
 
+	jsonData, e := json.Marshal(message)
+	if e != nil {
+		log.Printf("Error marshalling message: %v", e)
+		err <- e
+
+		return
+	}
+
+	var msg Message
+	e = json.Unmarshal(jsonData, &msg)
+	if e != nil {
+		log.Printf("Error unmarshalling message: %v", e)
+		err <- e
+
+		return
+	}
+
+	log.Printf("Sum of %d and %d is %d", msg.ID, msg.Number, msg.ID+msg.Number)
+
 	err <- nil
+
+	return
 }
