97f0d46a589bf270325c688fa0c7ea154498fd17
vessaldaneshvar <52780555+vessaldaneshvar@users.noreply.github.com>
2023-10-10 17:05:09 +0330

    feat: add redis transport (#14)
    * add redis transporter
    
    * add redis to action test

---
+++ redis_transport_test.go
@@ -0,0 +1,58 @@
+package gosumer
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRedisConnect(t *testing.T) {
+	transport := Redis{
+		Host:    "localhost",
+		Port:    6379,
+		Channel: "channel_name",
+	}
+
+	err := transport.connect()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
+func TestRedisListen(t *testing.T) {
+	transport := Redis{
+		Host:     "localhost",
+		Port:     6379,
+		User:     "",
+		Password: "",
+		Channel:  "channel_1",
+		DB:       0,
+	}
+
+	go func() {
+		err := transport.listen(processMessage, Message{})
+		if err != nil {
+			t.Errorf("Expected no error, got %v", err)
+		}
+	}()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	time.Sleep(1 * time.Second)
+
+	ctx := context.Background()
+	body := `{"id": 1, "name": "John Doe"}`
+	res := rdb.Publish(ctx, "channel_1", body)
+
+	if res.Err() != nil {
+		t.Errorf("Expected no error, got %v", res.Err())
+	}
+	time.Sleep(1 * time.Second)
+
+	assert.True(t, processMessageCalled, "Expected processMessage to be called")
+	processMessageCalled = false
+}
